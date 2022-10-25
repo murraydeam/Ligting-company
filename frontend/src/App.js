844eeaf64a966471536f6675d429468a8f0728d7
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-10-24 23:32:50 -0400

    Base webpage has been created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import Navi from "./components/Nav/Navi";
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navi />
+      <header className="App-header">
+
+      </header>
+      <Footer />
     </div>
   );
 }
